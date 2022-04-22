b9452218eacb2931fcee71bf5cd06edcd88c7a65
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 12:28:00 +0400

    feat: created flex component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className='App'>
-      <Box as='section' bg='green' w='120px' h='150px'>
-        This is a box
-      </Box>
+      <Flex justify='space-between' align='center'>
+        <Box bg='green' w='100px' h='200px'>
+          item1
+        </Box>
+        <Box bg='blue' w='100px' h='200px'>
+          item2
+        </Box>
+        <Box bg='red' w='200px' h='100px'>
+          item3
+        </Box>
+        <Box bg='tomato' w='100px' h='200px'>
+          item4
+        </Box>
+      </Flex>
     </div>
   );
 }
