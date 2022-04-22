826354321a1c21a123d82a717e6635ec52780ac8
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 22:23:48 +0400

    feat: created list item component

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,22 +1,57 @@
-import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  StackProps,
+  Text,
+} from '@chakra-ui/react';
+import { CheckIcon } from '../icons/Icons';
+
+export const ListItem = (props: StackProps) => {
+  const { children, ...rest } = props;
+  return (
+    <HStack as='li' spacing='20px' {...rest}>
+      <Icon as={CheckIcon} w='22px' h='22px' />
+      <Text>{children}</Text>
+    </HStack>
+  );
+};
 
 export function Pricing() {
   return (
-    <Flex textAlign='center'>
-      <Box bg='#F0EAFB' p='60px'>
-        <Text fontSize='24px' fontWeight='800'>
-          Premium PRO
-        </Text>
-        <Heading as='h3' fontSize='60px' mt='16px'>
-          $329
-        </Heading>
-        <Text color='#171923' fontSize='18px' fontWeight='500' mt='8px'>
-          billed just once
-        </Text>
-        <Button colorScheme='purple' size='lg' w='282px' mt='24px'>
-          Get Started
-        </Button>
-      </Box>
-    </Flex>
+    <Box
+      maxW='994px'
+      margin='auto'
+      mt='-160px'
+      borderRadius='12px'
+      overflow='hidden'
+      boxShadow='0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'
+    >
+      <Flex>
+        <Box bg='#F0EAFB' p='60px'>
+          <Text fontSize='24px' fontWeight='800'>
+            Premium PRO
+          </Text>
+          <Heading as='h3' fontSize='60px' mt='16px'>
+            $329
+          </Heading>
+          <Text color='#171923' fontSize='18px' fontWeight='500' mt='8px'>
+            billed just once
+          </Text>
+          <Button colorScheme='purple' size='lg' w='282px' mt='24px'>
+            Get Started
+          </Button>
+        </Box>
+        <Box p='60px' fontSize='18px' bg='white'>
+          <Text textAlign='left'>
+            Access these features when you get this pricing package for your
+            business.
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
