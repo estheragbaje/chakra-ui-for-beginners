d9cbba66d1a953267cc7ce5d6c99835501ca937a
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 22:53:16 +0400

    feat: used design token on pricing

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -14,7 +14,7 @@ import { CheckIcon } from '../icons/Icons';
 export const ListItem = (props: StackProps) => {
   const { children, ...rest } = props;
   return (
-    <HStack as='li' spacing='20px' {...rest}>
+    <HStack as='li' spacing='5' {...rest}>
       <Icon as={CheckIcon} w='22px' h='22px' />
       <Text>{children}</Text>
     </HStack>
@@ -26,8 +26,8 @@ export function Pricing() {
     <Box
       maxW='994px'
       margin='auto'
-      mt='-160px'
-      borderRadius='12px'
+      mt='-40'
+      borderRadius='xl'
       overflow='hidden'
       boxShadow='0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'
       textAlign='center'
@@ -35,25 +35,25 @@ export function Pricing() {
     >
       <Flex>
         <Box bg='#F0EAFB' p='60px'>
-          <Text fontSize='24px' fontWeight='800'>
+          <Text fontSize='xl' fontWeight='extrabold'>
             Premium PRO
           </Text>
-          <Heading as='h3' fontSize='60px' mt='16px'>
+          <Heading as='h3' fontSize='60px' mt='4'>
             $329
           </Heading>
-          <Text color='#171923' fontSize='18px' fontWeight='500' mt='8px'>
+          <Text color='gray.900' fontSize='lg' fontWeight='medium' mt='2'>
             billed just once
           </Text>
-          <Button colorScheme='purple' size='lg' w='282px' mt='24px'>
+          <Button colorScheme='purple' size='lg' w='282px' mt='6'>
             Get Started
           </Button>
         </Box>
-        <Box p='60px' fontSize='18px' bg='white'>
+        <Box p='60px' fontSize='lg' bg='white'>
           <Text textAlign='left'>
             Access these features when you get this pricing package for your
             business.
           </Text>
-          <Stack as='ul' spacing='20px' pt='24px'>
+          <Stack as='ul' spacing='5' pt='6'>
             <ListItem>International calling and messaging API</ListItem>
             <ListItem>Additional phone numbers</ListItem>
             <ListItem>Automated messages via Zapier</ListItem>
