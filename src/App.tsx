e2f963e189cbff222ef5fa2818f73b6485a17cd8
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 21:48:07 +0400

    feat: implemented pseudo style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className='App'>
-      <Box bg='green' h='300px' w='300px' margin='auto' mt='32px'>
+      <Box
+        bg='green'
+        h='300px'
+        w='300px'
+        margin='auto'
+        mt='32px'
+        textAlign='center'
+      >
         <Text fontSize='32px'>This is a style props example</Text>
+        <Button
+          mt='32px'
+          colorScheme='red'
+          _hover={{ background: 'blue' }}
+          _focus={{ background: 'yellow', color: 'black' }}
+        >
+          Click me
+        </Button>
       </Box>
     </div>
   );
