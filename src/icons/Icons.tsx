90a2f09da06f64c043c2fb4b8960dff464388c59
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 22:11:10 +0400

    feat: added check icion

---
+++ src/icons/Icons.tsx
@@ -0,0 +1,18 @@
+export function CheckIcon() {
+  return (
+    <svg
+      width='22'
+      height='22'
+      viewBox='0 0 22 22'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fill-rule='evenodd'
+        clip-rule='evenodd'
+        d='M11 22C17.0751 22 22 17.0751 22 11C22 4.92487 17.0751 0 11 0C4.92487 0 0 4.92487 0 11C0 17.0751 4.92487 22 11 22ZM6.2 10.0857L5 11.3714L9 16L17 8.28571L15.8 7L9 12.4L6.2 10.0857Z'
+        fill='#652CD3'
+      />
+    </svg>
+  );
+}
