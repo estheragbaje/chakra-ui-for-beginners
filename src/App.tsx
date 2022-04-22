bfbc92c5790976ec973549c6ba122162efaa1f77
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 21:24:06 +0400

    feat: created heading component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Text } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className='App'>
-      <Text as='i'>
-        Chakra UI is a simple, modular and accessible component library that
-        gives you the building blocks you need to build your React applications.
-      </Text>
+      <Heading as='h1' fontSize='54px'>
+        Create accessible React apps with speed
+      </Heading>
     </div>
   );
 }
