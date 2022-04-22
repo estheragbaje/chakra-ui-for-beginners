ce1e1eebd61202ade6901919defd49fed7b93c46
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 21:19:24 +0400

    feat: created text component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
-// To use HStack, VStack, remove the direction prop
 function App() {
   return (
     <div className='App'>
-      <Stack spacing='24px' direction='row'>
-        <Box bg='green' w='100px' h='200px'>
-          item1
-        </Box>
-        <Box bg='blue' w='100px' h='200px'>
-          item2
-        </Box>
-        <Box bg='red' w='200px' h='100px'>
-          item3
-        </Box>
-        <Box bg='tomato' w='100px' h='200px'>
-          item4
-        </Box>
-      </Stack>
+      <Text as='i'>
+        Chakra UI is a simple, modular and accessible component library that
+        gives you the building blocks you need to build your React applications.
+      </Text>
     </div>
   );
 }
