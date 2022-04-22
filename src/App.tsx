2d6a7b4000907c856d3bbd0668d6ae4a8c464a97
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-22 12:24:25 +0400

    feat: created box component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Box } from '@chakra-ui/react';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Box as='section' bg='green' w='120px' h='150px'>
+        This is a box
+      </Box>
+    </div>
+  );
 }
 
 export default App;
