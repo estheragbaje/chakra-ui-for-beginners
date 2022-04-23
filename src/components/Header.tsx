301a33e8a61ce35533c85e8078ea4b942a6abd43
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-23 12:07:06 +0400

    feat: added responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,15 @@ export function Header() {
       as='section'
       color='gray.50'
       bg='purple.600'
-      pt={['60px', '60px', '90px']}
+      pt={{ base: '60px', lg: '90px' }}
       pb='198px'
       px='8'
-      textAlign={['left', 'left', 'center']}
+      textAlign={{ base: 'left', lg: 'center' }}
     >
-      <Heading fontWeight='extrabold' fontSize={['3xl', '3xl', '5xl']}>
+      <Heading fontWeight='extrabold' fontSize={{ base: '3xl', lg: '5xl' }}>
         Simple pricing for your business
       </Heading>
-      <Text fontWeight='medium' fontSize={['lg', 'lg', '2xl']} pt='4'>
+      <Text fontWeight='medium' fontSize={{ base: 'lg', lg: '2xl' }} pt='4'>
         Plans that are carefully crafted to suit your business.
       </Text>
     </Box>
