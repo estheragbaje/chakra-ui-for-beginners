ee8136e9e2b3925df4e6990865e159abad82d931
Esther Agbaje <folasadeagbaje@gmail.com>
2022-04-23 12:11:24 +0400

    feat: added responsiveness to pricing

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -33,12 +33,12 @@ export function Pricing() {
         boxShadow='0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)'
         textAlign='center'
       >
-        <Flex direction={['column', 'column', 'row']}>
-          <Box bg='#F0EAFB' p={['28px', '6opx', '60px']}>
+        <Flex direction={{ base: 'column', lg: 'row' }}>
+          <Box bg='#F0EAFB' p={{ base: '28px', sm: '60px' }}>
             <Text fontSize='xl' fontWeight='extrabold'>
               Premium PRO
             </Text>
-            <Heading as='h3' fontSize={['5xl', '5xl', '6xl']} mt='4'>
+            <Heading as='h3' fontSize={{ base: '5xl', lg: '6xl' }} mt='4'>
               $329
             </Heading>
             <Text color='gray.900' fontSize='lg' fontWeight='medium' mt='2'>
@@ -47,13 +47,13 @@ export function Pricing() {
             <Button
               colorScheme='purple'
               size='lg'
-              w={['auto', '282px', '282px']}
+              w={{ base: 'auto', sm: '282px' }}
               mt='6'
             >
               Get Started
             </Button>
           </Box>
-          <Box p={['32px', '32px', '60px']} fontSize='lg' bg='white'>
+          <Box p={{ base: '32px', lg: '60px' }} fontSize='lg' bg='white'>
             <Text textAlign='left'>
               Access these features when you get this pricing package for your
               business.
